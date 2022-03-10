6118e394e3d8b4df8212ab00126fb3633196e84a
jorgegp998 <jorgegp@live.cl>
2022-03-10 07:47:10 -0300

    calcular el area de un cuadrado con numeros flotantes

---
+++ squareArea/squareArea/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SquareArea
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            //this program calculation rectangle area
+            float sideA, sideB, square_area;
+            Console.WriteLine("input a number: ");
+            sideA = float.Parse(Console.ReadLine());
+            Console.WriteLine("input a second number: ");
+            sideB = float.Parse(Console.ReadLine());
+            square_area = sideA * sideB;
+            Console.WriteLine("the square area is: " + square_area);
+        }
+    }
+}

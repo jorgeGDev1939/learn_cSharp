ff5547e5a9ac6c08cd79363693e48003c6187ba0
jorgegp998 <jorgegp@live.cl>
2022-03-10 06:58:39 -0300

    create a exercise with strings

---
+++ StringsExercise/StringsExercise/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int height = 173;
+int age = 24;
+string name = "Jorge";
+string lastName = "Galleguillos";
+string information = "My name is :" + name + lastName + ", I have" + age + "years old. " + "\n i have " + height + "cm";
+Console.WriteLine(information);

ce6afd4857a4ff1d4e7d8338f4b47373b473311f
jorgegp998 <jorgegp@live.cl>
2022-03-10 06:03:08 -0300

    how use a write line

---
+++ StringExample/StringExample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StringExample
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("What's your name? Please Write your full name");
+            var fullUserName = Console.ReadLine();
+
+            Console.WriteLine("Hello " + fullUserName + " Welcome to Platzi");
+        }
+    }
+}
